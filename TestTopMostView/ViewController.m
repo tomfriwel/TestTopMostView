0dd15afb128f7cfc50120b7e5ebd55854da24e5b
tomfriwel <976932447@qq.com>
2017-04-30 22:32:08 +0800

    add alert window

--- TestTopMostView/ViewController.m
+++ TestTopMostView/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) UIWindow *alertWindow;
+
+
 @end
 
 @implementation ViewController
@@ -20,6 +23,15 @@
 }
 
 - (IBAction)showAction:(id)sender {
+    id vc = [[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"MyAlertViewController"];
+//    [UIApplication sharedApplication].delegate.window.rootViewController = vc;
+//    
+    UIWindow *alertWindow = [[UIWindow alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 200)];
+    alertWindow.backgroundColor = [UIColor redColor];
+    alertWindow.windowLevel = UIWindowLevelAlert;
+    alertWindow.rootViewController = vc;
+    self.alertWindow = alertWindow;
+    [alertWindow makeKeyAndVisible];
 }
 
 
