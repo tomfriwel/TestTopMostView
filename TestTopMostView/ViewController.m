e2148bbd1f4be1964dccecefc48d1e495e2d553f
tomfriwel <976932447@qq.com>
2017-04-30 22:23:27 +0800

    init

--- TestTopMostView/ViewController.m
+++ TestTopMostView/ViewController.m
@@ -19,10 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)showAction:(id)sender {
 }
 
 
